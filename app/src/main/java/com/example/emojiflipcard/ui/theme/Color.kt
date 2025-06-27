f6d2bb237dd47b8d543fc4bd881ed93e74357f84
23BCSB66 <beheraasish2004@gmail.com>
2025-06-27 16:03:31 +0530

    Emoji

--- app/src/main/java/com/example/emojiflipcard/ui/theme/Color.kt
+++ app/src/main/java/com/example/emojiflipcard/ui/theme/Color.kt
@@ -2,10 +2,10 @@ package com.example.emojiflipcard.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
+val Blue80 = Color(0xFF008AFF)
+val BlueGrey80 = Color(0xFF868EC4)
 val Pink80 = Color(0xFFEFB8C8)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
+val Blue40 = Color(0xFF003AFF)
+val BlueGrey40 = Color(0xFF4E5685)
 val Pink40 = Color(0xFF7D5260)
